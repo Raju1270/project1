ffd92f2e4595c6fbeba047990669f1aa7bb56296
Raju <mauryaraju306@gmail.com>
2023-05-29 18:21:19 +0530

    1
    1

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+function Navbar() {
+  return (
+    <div className=" absolute w-full p-7 flex justify-between items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex gap-5">
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/product">Product</Link>
+        </li>
+      </ul>
+
+
+      <ul className="flex gap-5">
+        <li>
+          <Link href="/login">login</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+       
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="h-96 w-96 bg-slate-900 border rounded-md"> form</div>
+    </div>
+  );
+}
+
+export default Login;
