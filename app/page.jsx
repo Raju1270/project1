f9aa705bb0227c2214db2aa23b68048c9a12ad77
Raju <mauryaraju306@gmail.com>
2023-07-14 10:58:30 +0530

    2

--- app/page.jsx
+++ app/page.jsx
@@ -6,25 +6,25 @@ export default function Home() {
     return (
         <div className="relative w-full  px-10 pt-24 md:p-24" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, ease: "easeOut" }} >
             <svg
-                className="star absolute top-6 left-52 opacity-[0.5] invisible lg:visible"
+                className="star absolute top-6 left-52 opacity-[0.5] invisible lg:visible fill-purple-400"
                 width="119"
                 height="134"
                 viewBox="0 0 89 114"
             >
                 <path
                     d="M44.5 0L46.5948 16.8652C49.0848 36.9114 64.3837 52.9193 84.1901 56.2026L89 57L84.1901 57.7974C64.3837 61.0807 49.0848 77.0886 46.5948 97.1348L44.5 114L42.4052 97.1348C39.9152 77.0886 24.6163 61.0807 4.8099 57.7973L0 57L4.80991 56.2026C24.6163 52.9193 39.9152 36.9114 42.4052 16.8652L44.5 0Z"
-                    fill="white"
+                    
                 />
             </svg>
             <svg
-                className="star absolute top-[45%] right-52 opacity-[0.5] invisible lg:visible"
+                className="star absolute top-[45%] right-52 opacity-[0.5] invisible lg:visible fill-purple-400"
                 width="80"
                 height="114"
                 viewBox="0 0 89 114"
             >
                 <path
                     d="M44.5 0L46.5948 16.8652C49.0848 36.9114 64.3837 52.9193 84.1901 56.2026L89 57L84.1901 57.7974C64.3837 61.0807 49.0848 77.0886 46.5948 97.1348L44.5 114L42.4052 97.1348C39.9152 77.0886 24.6163 61.0807 4.8099 57.7973L0 57L4.80991 56.2026C24.6163 52.9193 39.9152 36.9114 42.4052 16.8652L44.5 0Z"
-                    fill="white"
+                    
                 />
             </svg>
 
